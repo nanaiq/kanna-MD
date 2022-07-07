0e498e34aed4404231e55838c16262badb9f2188
nanaiq <108739585+nanaiq@users.noreply.github.com>
2022-07-07 21:08:00 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
-let text = `*${htki} MAU NGAPAIN OM ${htka}*
+let text = `*${htki} KONTOL ${htka}*
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: '🐈 My Github', url: sgh}},
-    {index: 2, urlButton: {displayText: '🌎 Official Group', url: sgc}},
+    {index: 1, urlButton: {displayText: 'Github', url: sgh}},
+    {index: 2, urlButton: {displayText: 'Official Group', url: sgc}},
     {index: 3, quickReplyButton: {displayText: 'Menu', id: '.menu'}},
     {index: 4, quickReplyButton: {displayText: 'Donasi', id: '.donasi'}},
 ]
